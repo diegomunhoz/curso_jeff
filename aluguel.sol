480302746cfca5e9a71b6fe5cf28786a92529d4f
Diego Munhoz <diego.munhoz@7comm.com.br>
2023-06-10 15:09:06 -0300

    inclusão de comentários

--- aluguel.sol
+++ aluguel.sol
@@ -7,10 +7,16 @@ pragma solidity 0.8.20;
 
 contract Aluguel {
 
+    // Nome do locador
     string public nomeLocador;
+
+    // Nome do locatario
     string public nomeLocatario;
+
+    // Array dos valores do aluguel de 36 meses
     uint256[36] public valorAluguel;
 
+    // Construtor padrão
     constructor(string memory locador, string memory locatario, uint aluguel)  {
         nomeLocador = locador;
         nomeLocatario = locatario;
@@ -19,22 +25,27 @@ contract Aluguel {
         }
     }
 
+    // Função para buscar o valor do aluguel em um mês específico
     function buscaValorAluguel(uint mes) public view returns (uint) {
         return valorAluguel[mes-1];
     }
 
+    // Função para buscar o locador e locatário
     function buscaLocadorLocatario() public view returns (string memory, string memory) {
         return (getLocatario(), getLocador());
     }
 
+    // Função get que retorna o locador com texto concatenado
     function getLocador() public view returns (string memory) {
-        return string.concat("Locador :", nomeLocador);
+        return string.concat("Locador: ", nomeLocador);
     }
 
+    // Método get que retorna o locatário com texto concatenado
     function getLocatario() public view returns (string memory) {
-        return string.concat("Locatario :", nomeLocatario);
+        return string.concat("Locatario: ", nomeLocatario);
     }
 
+    // Função para alterar o nome com base no tipo de pessoa recebido (1-LOCADOR / 2-LOCATÁRIO)
     function alterarNome(uint tipoPessoa, string memory novoNome) public {
         if (tipoPessoa == 1) {
             nomeLocador = novoNome;
@@ -45,6 +56,7 @@ contract Aluguel {
         }
     }
 
+    // Função para efetuar reajuste do valor do aluguel recebendo o mês incial e valor
     function aplicaAjusteAluguel(uint256 mesInicial, uint256 valor) public {
         if (mesInicial > 36) {
             revert("Mes invalido");
