57b5b3cb21152b6da5f883d1ed77a4d9bc050322
Diego Munhoz <diego.munhoz@7comm.com.br>
2023-06-12 18:53:55 -0300

    criação contrato livro-bordo.sol

---
+++ livro-bordo.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract Status {
+    bool public isValid;
+    constructor() {
+        isValid = true;
+    }
+    function notValid() public returns (bool) {
+        isValid = false;
+        return true;
+    }
+}
+
+contract Ownable {
+    address public owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can perform this action");
+        _;
+    }
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    function changeOwner(address _owner) public onlyOwner returns (bool) {
+        owner = _owner;
+        return true;
+    }
+
+}
+
+contract LivroDeBordo is Ownable, Status {
+    mapping (bytes32 => string message) public ownerMessages;
+
+    function saveMessage(string calldata _msg) external onlyOwner returns (bytes32) {
+        ownerMessages[keccak256(bytes(_msg))] = _msg;
+        return keccak256(bytes(_msg));
+    }
+}
